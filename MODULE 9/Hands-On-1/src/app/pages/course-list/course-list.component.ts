3f012dd4d49f90da0fb9ac107f00b8530a3a09e7
Vishwesh <vishwesh.rj@gmail.com>
2026-07-24 22:35:23 +0530

    Add files via upload

---
+++ MODULE 9/Hands-On-1/src/app/pages/course-list/course-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-list',
+  standalone: true,
+  template: '<p>Course List Page</p>'
+})
+export class CourseListComponent {}
---
+++ MODULE 9/Hands-On-1/src/app/pages/student-profile/student-profile.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-profile',
+  standalone: true,
+  template: '<p>Student Profile Page</p>'
+})
+export class StudentProfileComponent {}
